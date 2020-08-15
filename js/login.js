23a3cd3840e3169ff313cd254e255548f1b3403f
Facundo Bentos <facubentos@gmail.com>
2020-08-15 13:54:16 -0300

    Primer entrega

--- js/login.js
+++ js/login.js
@@ -1,5 +1,48 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function(e){ 
+
 });
+
+function validate(){
+    let campouno = document.getElementById("name").value;
+    let contenido = "";
+
+    if(campouno == "" || campouno == null){
+
+        contenido +=`
+        <p>*Debe ingresar un usuario*</p>
+        <br>
+        `
+        document.getElementById("erroruser").innerHTML = contenido;
+    return false;
+    
+}
+    let campodos = document.getElementById("pass").value;
+    let inpass = "";
+
+    if(campodos == "" || campodos == null){
+
+        inpass += `
+        <p>*Debe ingresar una contraseña*</p>
+        <br>
+        `
+        document.getElementById("errorpass").innerHTML = inpass;
+        return false;
+}
+}
+
+function enviar(){
+    if(validate() == false) {
+        return validate();
+    }else{
+        return link() 
+    }
+}
+
+function link(){
+    if(!validate()){
+        return location.href="index.html"
+    }
+}
