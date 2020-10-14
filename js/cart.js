6b7c484cd9e12ca9c16b7171537d6f7c74814b59
Facundo Bentos <facubentos@gmail.com>
2020-10-14 19:10:46 -0300

    sexta entrega

--- js/cart.js
+++ js/cart.js
@@ -25,7 +25,7 @@ function showCart(array){
                      </div>
                      <div class="col-6 col-sm-3">
                         <p class="text-muted">Cantidad</p>
-                        <input class="col-md-8" type="number" name="cantidad" min="1" placeholder="`+ articulos.count +`" onchange="mostrarTotal(`+ articulos.unitCost +`, this.value, `+ i +`)">   
+                        <input class="col-md-8" type="number" id="input`+i+`" name="cantidad" min="1" placeholder="`+ articulos.count +`" onchange="mostrarTotal(`+ articulos.unitCost +`, this.value, `+ i +`)">   
                      </div>
                    </div>
                 </div>
@@ -71,14 +71,77 @@ function cambioMoneda(moneda, costo, i){
 // También agregué como placeholder el valor predeterminado del JSON que sería cantidad 2, por lo tanto un total de 200
 
 
+//Cargar el JSON en el modal
+
+var data = {};
+
+function showModal(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array["articles"].length; i++){
+        let art = array["articles"][i];
+
+        htmlContentToAppend += `
+
+        <div class="row justify-content-end">
+          <small class="col-md-offset-11 col-md-1 mr-3" style="color: red;">Eliminar</small>
+        </div>
+        <div class="row mb-3">
+              <img src="` + art.src +`" class="img-thumbnail col-2" alt="foto`+i+`">
+
+               <div class="col-6 list-group-item">
+                  <h4>`+ art.name + `</h4>
+                   <div class="row py-1">
+                     <div class="col-6 col-sm-9">
+                        <p class="text-muted">Descripción</p>
+                     </div>
+                     <div class="col-6 col-sm-3">
+                        <p class="text-muted">Cantidad</p>
+                        <input class="col-md-8" onload="recibirInput()" type="number" id="mostrar`+i+`" name="cantidad" min="1" placeholder="`+ art.count +`">   
+                     </div>
+                   </div>
+                </div>
+         
+           <div class="col-2 list-group-item">
+                  <h5 class="text-center">Valor unitario</h5>
+               <div class="py-4 ml-4">
+                 <span>`+ art.unitCost + `</span>
+                 <span>`+ art.currency + `</span>
+               </div>
+            </div>
+
+           <div class="col-2 list-group-item"> 
+              <h5 class="text-center">Total</h5>
+              <div class="py-4 ml-4">
+              <span id="parr`+i+`">` + art.count*art.unitCost +`</span>
+              <span id="curren`+i+`">`+ art.currency + `</span>
+              </div>
+           </div> 
+     </div>
+        `
+    }
+    document.getElementById("modal").innerHTML = htmlContentToAppend;
+}
+
+// Función para mandar el valor del input al modal, en la sección cantidad
+
+function recibirInput(){
+   var valor = document.getElementById("input"+i).value;
+   document.getElementById("mostrar"+i).value = valor;
+}
+
+
+
 //agregué el JSON con los dos productos en una nueva constante llamada CART INFO2 URL
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(CART_INFO2_URL).then(function(resultObj){
         if (resultObj.status === "ok")
         {
-            //Muestro
+            //Muestro ambos json.
             info = resultObj.data;
+            data = resultObj.data;
             showCart(info);
+            showModal(data);
         }
     });
 }); 
